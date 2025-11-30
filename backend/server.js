08d051f5b936ce8c241b22c50601a2ab566daa27
Rajvi <rajvikhatri2003@gmail.com>
2025-12-01 01:51:25 +0530

    Update CORS origin to allow multiple sources

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,11 @@ connectDB();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [
+    "http://localhost:3000",              
+    "https://eventhub-backend-k9ir.onrender.com/api"  
+  ],
+    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
